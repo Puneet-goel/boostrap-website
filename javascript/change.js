9267be45729e00194a1af79bafad646b4e8d03db
Puneet-goel <puneetgoel016@gmail.com>
2021-07-15 23:21:14 +0530

    Responsive Navbar Carousel

---
+++ javascript/change.js
@@ -0,0 +1,6 @@
+
+
+// margin of carousel
+// Setting margin for carousel by getting navbar height 
+divElement = document.getElementById("header").offsetHeight;
+$("#example").css("margin-top",`${divElement}px`); 
