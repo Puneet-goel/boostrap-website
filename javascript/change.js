4d20fcfb3e9a6c8ed5e2389e78b21eb66f871573
Puneet-goel <puneetgoel016@gmail.com>
2021-07-16 20:36:59 +0530

    Search Page

--- javascript/change.js
+++ javascript/change.js
@@ -1,6 +1,15 @@
 
-
-// margin of carousel
-// Setting margin for carousel by getting navbar height 
 divElement = document.getElementById("header").offsetHeight;
+
+// margin for carousel
+// Setting margin by getting navbar height 
 $("#example").css("margin-top",`${divElement}px`); 
+
+
+// margin for search button
+// Setting margin by getting navbar height 
+$("#search-button").css("margin-top",`${divElement+40}px`); 
+
+function showAlert() {
+    alert(document.getElementById("user-search").value);
+}
