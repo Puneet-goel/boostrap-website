9d34e52ac566cb6c8888fc2d575fa41372bb6029
Puneet-goel <puneetgoel016@gmail.com>
2021-07-17 18:09:48 +0530

    Tooltip

--- javascript/change.js
+++ javascript/change.js
@@ -1,10 +1,21 @@
+function adjust(){
+	let divElement = document.getElementById("header").offsetHeight;
 
-let divElement = document.getElementById("header").offsetHeight;
+	// margin for carousel
+	// Setting margin by getting navbar height 
+	$("#example").css("margin-top",`${divElement}px`); 
+}
 
-// margin for carousel
-// Setting margin by getting navbar height 
-$("#example").css("margin-top",`${divElement}px`); 
 
+function toolTip(){
+	$(document).ready(function(){
+		$('[data-toggle="entry"]').tooltip();   
+	});
+	$(document).ready(function(){
+		$('[data-toggle="exit"]').tooltip();   
+	});
+
+}
 
 //Q idk y arrow function is not working here
 function showAlert(){
@@ -13,4 +24,15 @@ function showAlert(){
 
 const purchase = () => {
 	 alert("No items are added");
+}
+
+function filter(){
+	$(document).ready(function(){
+		$("#search-input").on("keyup",function(){
+			var value = $(this).val().toLowerCase();
+			$("#myTable tr").filter(function(){
+				$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+			});
+		});
+    });
 }
